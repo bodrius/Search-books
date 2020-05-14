499fe7036abf22ad1c383adfb51a9433047e34e2
Bogdan Pechenyk <bogdanpechenyk@gmail.com>
2020-05-15 00:19:37 +0300

    create build

--- webpack/config.js
+++ webpack/config.js
@@ -5,13 +5,3 @@ const loadModeConfig = env => require(`./configs/${env.mode}`)(env);
 
 module.exports = env =>
   webpackMerge(loadSharedConfig(env), loadModeConfig(env));
-
-  {
-    ...
-    module: {
-      rules: [
-        ...
-        { test: /\.hbs$/, exclude: /node_modules/, use: "handlebars-loader" }
-      ]
-    }
-  }
